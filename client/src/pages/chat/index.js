8f17484506472cc0dd761fcb470eb686d6edc73a
Marck Patricks <87098802+MarckPatricks@users.noreply.github.com>
2023-05-22 12:09:13 -0600

    Update comments in index.js

--- client/src/pages/chat/index.js
+++ client/src/pages/chat/index.js
@@ -1,18 +1,21 @@
 
-import styles from './styles.module.css';
-import RoomAndUsersColumn from './room-and-users';
-import SendMessage from './send-message';
-import MessagesReceived from './messages';
+import styles from './styles.module.css'; // Importa los estilos del módulo CSS correspondiente al componente
+import RoomAndUsersColumn from './room-and-users'; // Importa el componente RoomAndUsersColumn desde su archivo
+import SendMessage from './send-message'; // Importa el componente SendMessage desde su archivo
+import MessagesReceived from './messages'; // Importa el componente MessagesReceived desde su archivo
 
-const Chat = ({ username, room, socket }) => {
+const Chat = ({ username, room, socket }) => { 
   return (
-    <div className={styles.chatContainer}>
+    <div className={styles.chatContainer}> // Define un div con la clase CSS chatContainer
 
-      <RoomAndUsersColumn socket={socket} username={username} room={room} />
+      <RoomAndUsersColumn socket={socket} username={username} room={room} /> // Renderiza el componente RoomAndUsersColumn 
+//y le pasa las propiedades socket, username y room
 
       <div>
-        <MessagesReceived socket={socket} />
-        <SendMessage socket={socket} username={username} room={room} />
+        <MessagesReceived socket={socket} /> // Renderiza el componente MessagesReceived y le pasa la propiedad socket
+        <SendMessage socket={socket} username={username} room={room} /> // Renderiza el componente SendMessage y le pasa las propiedades
+// socket, username y room
+      </div>
       </div>
     </div>
   );
